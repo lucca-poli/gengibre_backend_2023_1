3b1d23bea7de421eff08877d961059531f3e273d
lucca-poli <lucca10azevedo@usp.br>
2023-04-03 15:28:59 -0300

    Added config for eslint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: "hello world!"})
-})
+app.get("/", (request, response) => {
+    return response.json({ message: "hello world!" });
+});
 
 app.listen(3333, () => {
     console.log("Server started on port 3333!");
